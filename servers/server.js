8146a8577da2f5db37ae3c3d8571a7efd0bf5408
justice jennings <justice.l.jennings@gmail.com>
2023-12-06 00:14:51 -0500

    added function to server file.

--- servers/server.js
+++ servers/server.js
@@ -1,6 +1,17 @@
 const http = require(`http`)
-const app = express()
 
-http.createServer((req, res) => {
-res.end(`working`);
-}).listen(3000)
+
+// http.createServer((req, res) => {
+// res.end(`working`);
+// }).listen(3000)
+
+const express = require('express');
+const app = express();
+
+app.get('/,', (req, res) => {
+res.send(
+    '<h1>welcome</h1>'
+)
+})
+
+app.listen(3000);
